def8b3ee76aa8ecdb244ae98981defea95e18371
Piyush9563 <85403636+Piyush9563@users.noreply.github.com>
2024-07-20 20:24:54 +0530

    Create Lecture20.js

---
+++ Lecture20.js
@@ -0,0 +1,16 @@
+function Pizza(props) {
+  return (
+    // Functional Components
+    //Props - properties object props.property
+    // JSX - Javascript XML
+    // React.createElement(div, ...)
+
+    <div class="pizza">
+      <h1>{props.name}</h1>
+    </div>
+  );
+}
+
+//function(Components), where to add on the DOM
+ReactDOM.render(<Pizza name="PizzaHut" />, document.getElementById("root"));
+ReactDOM.render(<Pizza name="Domino's" />, document.getElementById("root2"));
